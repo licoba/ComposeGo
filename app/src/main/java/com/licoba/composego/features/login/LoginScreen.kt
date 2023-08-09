6ea43f6b24f891bc9e4982aeafdf362f606edffe
licoba <licoba@qq.com>
2023-08-09 18:17:31 +0800

    feat：登陆页

--- app/src/main/java/com/licoba/composego/features/login/LoginScreen.kt
+++ app/src/main/java/com/licoba/composego/features/login/LoginScreen.kt
@@ -6,6 +6,11 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
@@ -13,6 +18,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.licoba.composego.R
 import com.licoba.composego.core.viewmodel.LoginUiInfo
 import com.licoba.composego.core.viewmodel.LoginViewModel
 import com.licoba.composego.ui.theme.AppTheme
@@ -51,11 +57,18 @@ fun LoginScreen(
         verticalArrangement = Arrangement.Top
     ) {
         Spacer(modifier = Modifier.height(100.dp))
-        InputTextField(text = loginUiInfo.userName) {
+        InputTextField(
+            icon =  Icons.Default.AccountBox,
+            label = stringResource(R.string.login_user_name),
+            text = loginUiInfo.userName
+        ) {
             onUserNameChanged(it)
         }
         Spacer(modifier = Modifier.height(10.dp))
-        InputTextField(text = loginUiInfo.password) {
+        InputTextField(
+            icon =  Icons.Default.Lock,
+            label = stringResource(R.string.login_password),
+            text = loginUiInfo.password) {
             onPasswordChanged(it)
         }
         Spacer(modifier = Modifier.height(20.dp))
