0c02cb7ea628c677788ebfd62ab8558a0575bf6a
licoba <licoba@qq.com>
2023-08-11 13:33:40 +0800

    fix：补齐所有颜色的调色板

--- app/src/main/java/com/licoba/composego/ui/theme/Color.kt
+++ app/src/main/java/com/licoba/composego/ui/theme/Color.kt
@@ -5,74 +5,137 @@ import androidx.compose.ui.graphics.Color
 /**
  * App colors.
  */
+// 蓝色
 internal val Blue10 = Color(0xFF001F29)
 internal val Blue20 = Color(0xFF003544)
 internal val Blue30 = Color(0xFF004D61)
 internal val Blue40 = Color(0xFF006781)
+internal val Blue50 = Color(0xFF007FA1)
+internal val Blue60 = Color(0xFF0099C2)
+internal val Blue70 = Color(0xFF00B3E2)
 internal val Blue80 = Color(0xFF5DD4FB)
 internal val Blue90 = Color(0xFFB5EAFF)
 internal val Blue95 = Color(0xFFDCF5FF)
 
+// 深绿色
 internal val DarkGreen10 = Color(0xFF0D1F12)
 internal val DarkGreen20 = Color(0xFF223526)
 internal val DarkGreen30 = Color(0xFF394B3C)
 internal val DarkGreen40 = Color(0xFF4F6352)
+internal val DarkGreen50 = Color(0xFF667868)
+internal val DarkGreen60 = Color(0xFF7C8E7E)
+internal val DarkGreen70 = Color(0xFF939F94)
 internal val DarkGreen80 = Color(0xFFB7CCB8)
 internal val DarkGreen90 = Color(0xFFD3E8D3)
 
+// 深绿灰
 internal val DarkGreenGray10 = Color(0xFF1A1C1A)
+internal val DarkGreenGray20 = Color(0xFF313431)
+internal val DarkGreenGray30 = Color(0xFF494C49)
+internal val DarkGreenGray40 = Color(0xFF616663)
+internal val DarkGreenGray50 = Color(0xFF787C78)
+internal val DarkGreenGray60 = Color(0xFF909590)
+internal val DarkGreenGray70 = Color(0xFFA8ACA8)
+internal val DarkGreenGray80 = Color(0xFFC0C4C0)
 internal val DarkGreenGray90 = Color(0xFFE2E3DE)
 internal val DarkGreenGray95 = Color(0xFFF0F1EC)
 internal val DarkGreenGray99 = Color(0xFFFBFDF7)
-internal val DarkPurpleGray10 = Color(0xFF201A1B)
-internal val DarkPurpleGray90 = Color(0xFFECDFE0)
-internal val DarkPurpleGray95 = Color(0xFFFAEEEF)
-internal val DarkPurpleGray99 = Color(0xFFFCFCFC)
 
+// 绿色
 internal val Green10 = Color(0xFF00210B)
 internal val Green20 = Color(0xFF003919)
 internal val Green30 = Color(0xFF005227)
 internal val Green40 = Color(0xFF006D36)
+internal val Green50 = Color(0xFF008545)
+internal val Green60 = Color(0xFF00A353)
+internal val Green70 = Color(0xFF00BB61)
 internal val Green80 = Color(0xFF0EE37C)
 internal val Green90 = Color(0xFF5AFF9D)
+
+
+// 绿灰色
+internal val GreenGray10 = Color(0xFF272F28)
+internal val GreenGray20 = Color(0xFF3E463D)
 internal val GreenGray30 = Color(0xFF414941)
+internal val GreenGray40 = Color(0xFF5A6259)
 internal val GreenGray50 = Color(0xFF727971)
 internal val GreenGray60 = Color(0xFF8B938A)
+internal val GreenGray70 = Color(0xFFA4ACA2)
 internal val GreenGray80 = Color(0xFFC1C9BF)
 internal val GreenGray90 = Color(0xFFDDE5DB)
 
+// 橙色
 internal val Orange10 = Color(0xFF390C00)
 internal val Orange20 = Color(0xFF5D1900)
 internal val Orange30 = Color(0xFF812800)
 internal val Orange40 = Color(0xFFA23F16)
+internal val Orange50 = Color(0xFFC25C33)
+internal val Orange60 = Color(0xFFE07950)
+internal val Orange70 = Color(0xFFF4966D)
 internal val Orange80 = Color(0xFFFFB599)
 internal val Orange90 = Color(0xFFFFDBCE)
 internal val Orange95 = Color(0xFFFFEDE6)
+
+// 紫灰色
 
 internal val Purple10 = Color(0xFF36003D)
 internal val Purple20 = Color(0xFF560A5E)
 internal val Purple30 = Color(0xFF702776)
 internal val Purple40 = Color(0xFF8C4190)
+internal val Purple50 = Color(0xFFA95CA9)
+internal val Purple60 = Color(0xFFC379C3)
+internal val Purple70 = Color(0xFFD89FD8)
 internal val Purple80 = Color(0xFFFFA8FF)
 internal val Purple90 = Color(0xFFFFD5FC)
 internal val Purple95 = Color(0xFFFFEBFB)
 
+
+// 紫灰色
+internal val PurpleGray10 = Color(0xFF3B3139)
+internal val PurpleGray20 = Color(0xFF574D55)
 internal val PurpleGray30 = Color(0xFF4E444C)
+internal val PurpleGray40 = Color(0xFF6B6169)
 internal val PurpleGray50 = Color(0xFF7F747C)
 internal val PurpleGray60 = Color(0xFF998D96)
+internal val PurpleGray70 = Color(0xFFB3A7AF)
 internal val PurpleGray80 = Color(0xFFD0C2CC)
 internal val PurpleGray90 = Color(0xFFEDDEE8)
 
+
+// 深紫灰
+internal val DarkPurpleGray10 = Color(0xFF201A1B)
+internal val DarkPurpleGray20 = Color(0xFF382E30)
+internal val DarkPurpleGray30 = Color(0xFF514548)
+internal val DarkPurpleGray40 = Color(0xFF694D60)
+internal val DarkPurpleGray50 = Color(0xFF82556D)
+internal val DarkPurpleGray60 = Color(0xFF9B5D85)
+internal val DarkPurpleGray70 = Color(0xFFB4659C)
+internal val DarkPurpleGray80 = Color(0xFFCD6DAF)
+internal val DarkPurpleGray90 = Color(0xFFECDFE0)
+internal val DarkPurpleGray95 = Color(0xFFFAEEEF)
+internal val DarkPurpleGray99 = Color(0xFFFCFCFC)
+
+// 红色
 internal val Red10 = Color(0xFF410001)
 internal val Red20 = Color(0xFF680003)
 internal val Red30 = Color(0xFF930006)
 internal val Red40 = Color(0xFFBA1B1B)
+internal val Red50 = Color(0xFFE03A36)
+internal val Red60 = Color(0xFFEB5954)
+internal val Red70 = Color(0xFFF27C7A)
 internal val Red80 = Color(0xFFFFB4A9)
 internal val Red90 = Color(0xFFFFDAD4)
+internal val Red95 = Color(0xFFFFEFE9)
 
+
+// 深绿色
 internal val Teal10 = Color(0xFF001F26)
 internal val Teal20 = Color(0xFF02363F)
 internal val Teal30 = Color(0xFF214D56)
 internal val Teal40 = Color(0xFF3A656F)
+internal val Teal50 = Color(0xFF538D88)
+internal val Teal60 = Color(0xFF6BA59F)
+internal val Teal70 = Color(0xFF83BDB7)
 internal val Teal80 = Color(0xFFA2CED9)
 internal val Teal90 = Color(0xFFBEEAF6)
+internal val Teal95 = Color(0xFFD9F2FA)
